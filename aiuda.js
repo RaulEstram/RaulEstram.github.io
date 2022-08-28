3326970120dfed073ee366d3790a05a2bc0f26d4
RaulEstram <ralesrtada2000@outloook.com>
2022-08-27 20:53:02 -0500

    2

--- aiuda.js
+++ aiuda.js
@@ -1,8 +1,8 @@
-function reqListener () {
-    console.log(this.responseText);
-  }
+// function reqListener () {
+//     console.log(this.responseText);
+//   }
   
-  var oReq = new XMLHttpRequest();
-  oReq.addEventListener("load", reqListener);
-  oReq.open("GET", "https://jsonplaceholder.typicode.com/todos");
-  oReq.send();
+//   var oReq = new XMLHttpRequest();
+//   oReq.addEventListener("load", reqListener);
+//   oReq.open("GET", "https://jsonplaceholder.typicode.com/todos");
+//   oReq.send();
